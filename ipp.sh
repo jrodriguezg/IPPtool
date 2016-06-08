fe1c0f9afbd2947ed8de101818d6488803b63580
jrodriguezg <johntherip@gmail.com>
2016-06-08 16:11:45 +0200

    Include requesting user in ipptool command through ipp.sh. Modify get_completed_jobs and get_processing_jobs to take into account requesting user.

--- ipp.sh
+++ ipp.sh
@@ -3,12 +3,12 @@
 IP="$1"
 VERSION="$2"
 FILE="$3"
-
+USER="$4"
 
 if [ "$#" -lt 3 ]; then
 	echo "Error: missing args."
 else
-	ipptool -4 -V $VERSION -t -v http://$IP:631/ipp/print $FILE
+	ipptool -4 -V $VERSION -t -v -d REQ_USER=$USER http://$IP:631/ipp/print $FILE
 fi
 
 
